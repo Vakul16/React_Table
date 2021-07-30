aba81696db806afe239f35fd83c0f66bc4d8b1e4
Vakul16 <valusavakhul@gmail.com>
2021-07-30 19:32:20 +0530

    initial commit

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Table from "../Components/Table";
+const HomePage = () => {
+  return (
+    <div>
+      HOMEPAGE
+      <Table />
+    </div>
+  );
+};
+
+export default HomePage;
